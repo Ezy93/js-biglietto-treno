3d70d32b2cd558f1b641b78dc4421c0c51686271
Ezy93 <giordani001@gmail.com>
2022-02-22 16:10:16 +0100

    completato funzionamento senza controlli

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const userAge = parseInt( prompt("inserisci la tua età"));
+console.log(userAge);
+
+const travelLength = parseInt(prompt("inserisci i km che vuoi percorrere"));
+console.log(travelLength);
+
+const kmPrice = 0.21;
+
+const fullTicketPrice = travelLength * kmPrice;
+console.log(fullTicketPrice.toFixed(2));
+
+let teenDiscount = fullTicketPrice * 20 / 100;
+let teenTicketPrice = "";
+let oldDiscount = fullTicketPrice * 40 / 100;
+let oldTicketPrice = "";
+
+if (userAge < 18) {
+
+    teenTicketPrice = fullTicketPrice - teenDiscount;
+    console.log(teenTicketPrice.toFixed(2));
+    
+}
+
+if (userAge > 65 ){
+    
+    oldTicketPrice = fullTicketPrice - oldDiscount;
+    console.log(oldTicketPrice.toFixed(2));
+}
+
+
+
+
+
+
